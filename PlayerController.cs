f2295a3546ef0f959779e3455108693698ba514f
elliotichi <e.priestley@rgu.ac.uk>
2025-03-08 05:22:53 +0000

    wall jumps and sprint added

--- PlayerController.cs
+++ PlayerController.cs
@@ -3,8 +3,10 @@ using System;
 
 public partial class PlayerController : CharacterBody2D
 {
-	private float speed = 300.0f;
+	private float speed = 150.0f;
+	private float sprintSpeed = 400.0f;
 	private float jumpSpeed = -400.0f;
+	private float wallJumpSpeed = 250.0f;
 	private float acceleration = .05f;
 	private float friction = .25f;
 	private bool sprinting;
@@ -22,13 +24,16 @@ public partial class PlayerController : CharacterBody2D
 	{
 		Vector2 velocity = Velocity;
 
-		// Add the gravity.
+		float direction = Input.GetAxis("left", "right");
+		float leftStrength = Input.GetActionStrength("left");
+		float rightStrength = Input.GetActionStrength("right");
 		velocity.Y += gravity * (float)delta;
+
+
 		if (IsOnFloor())
 		{
 			jumpCount = 2;
 		}
-
 
 		// Handle jump.
 		if (Input.IsActionJustPressed("jump") && jumpCount > 0)
@@ -37,26 +42,50 @@ public partial class PlayerController : CharacterBody2D
 			velocity.Y = jumpSpeed;
 		}
 
-		if (Input.IsActionJustPressed("sprint")){
+		if (Input.IsActionJustPressed("jump") && GetNode<RayCast2D>("RaycastLeft").IsColliding() && IsOnWallOnly()){
+			velocity.Y = jumpSpeed;
+			velocity.X = wallJumpSpeed;
+			GD.Print("i wall jumped!");
+			
+		}
+
+		if (Input.IsActionJustPressed("jump") && GetNode<RayCast2D>("RaycastRight").IsColliding() && IsOnWallOnly()){
+			velocity.Y = jumpSpeed;
+			velocity.X = -wallJumpSpeed;
+			GD.Print("i wall jumped!");
+		}
+
+		if (Input.IsActionJustPressed("sprint"))
+		{
 			sprinting = true;
 		}
 
-		// Get the input direction.
-		float direction = Input.GetAxis("left", "right");
-		float leftStrength = Input.GetActionStrength("left");
-		float rightStrength = Input.GetActionStrength("right");
+		if (direction != lastDirection && sprinting == true)
+		{
+			sprinting = false;
+		}
 
 		if (leftStrength == 0f && rightStrength == 0f && IsOnFloor())
 		{
 			velocity.X = Mathf.Lerp(velocity.X, 0, friction);
 			sprinting = false;
-		}
-		
-		else
-		{
-			velocity.X = Mathf.Lerp(velocity.X, direction * speed, acceleration);
+			
 		}
 
+		else
+		{
+			switch (sprinting)
+			{
+				case true:
+					velocity.X = Mathf.Lerp(velocity.X, direction * sprintSpeed, acceleration);
+					break;
+				default :
+					velocity.X = Mathf.Lerp(velocity.X, direction * speed, acceleration);
+					break;
+
+			}
+		}
+		lastDirection = direction;
 		Velocity = velocity;
 		MoveAndSlide();
 
